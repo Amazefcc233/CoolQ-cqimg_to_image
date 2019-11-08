0663c7c6fccf9e235cdc0dcb86e25ad4d380d404
Amazefcc233 <34246558+Amazefcc233@users.noreply.github.com>
2019-11-08 20:23:53 +0800

    edit

---
+++ nulledit.py
@@ -0,0 +1,23 @@
+# -*- coding: UTF-8 -*-
+
+import os
+
+def main():
+	concls = os.system('cls')
+	print ("null转jpg Start!")
+	proDir = os.path.split(os.path.realpath(__file__))[0]
+	todoPath = os.path.join(proDir, "./todo/")
+	okPath = os.path.join(proDir, "./ok/")
+
+	num = 1
+	for file in os.listdir(okPath):
+		filesex=file[-4:]
+		if filesex == "null":
+			try:
+				os.rename(os.path.join(okPath, file), os.path.join(okPath, "null_"+str(num)+".jpg"))
+				print ("null_"+str(num)+".jpg")
+			except FileExistsError as exists: # 如果文件存在，也不会报错，将继续执行
+				print ("文件名为null_"+str(num)+".jpg已存在，因此无法对文件"+file+"进行重命名操作。请修改原有名称，然后再试一次。")
+				continue
+			num += 1
+	print ("完成了哟！")
---
+++ tmpedit.py
@@ -0,0 +1,23 @@
+# -*- coding: UTF-8 -*-
+
+import os
+
+def main():
+	concls = os.system('cls')
+	print ("tmp转jpg Start!")
+	proDir = os.path.split(os.path.realpath(__file__))[0]
+	todoPath = os.path.join(proDir, "./todo/")
+	okPath = os.path.join(proDir, "./ok/")
+
+	num = 1
+	for file in os.listdir(okPath):
+		filesex=file[-3:]
+		if filesex == "tmp":
+			try:
+				os.rename(os.path.join(okPath, file), os.path.join(okPath, "tmp_"+str(num)+".jpg"))
+				print ("tmp_"+str(num)+".jpg")
+			except FileExistsError as exists: # 如果文件存在，也不会报错，将继续执行
+				print ("文件名为tmp_"+str(num)+".jpg已存在，因此无法对文件"+file+"进行重命名操作。请修改原有名称，然后再试一次。")
+				continue
+			num += 1
+	print ("完成了哟！")
